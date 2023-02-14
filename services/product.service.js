496bd3e2b85c6b15b6aa86505bb1e1190811de64
Misael Briseño <misaelbj.contacto@gmail.com>
2023-02-14 17:09:04 -0600

    first commit

---
+++ services/product.service.js
@@ -0,0 +1,76 @@
+const faker = require('faker');
+const boom = require('@hapi/boom')
+
+class ProductService {
+  constructor() {
+    this.products = [];
+    this.generate();
+  }
+
+  generate() {
+    const limit = 9;
+    for (let index = 0; index < limit; index++) {
+      this.products.push({
+        id: faker.datatype.uuid(),
+        name: faker.commerce.productName(),
+        price: parseInt(faker.commerce.price(), 10),
+        image: faker.image.imageUrl(),
+        isBlock: faker.datatype.boolean(),
+      });
+    }
+  }
+
+  async create(body) {
+    const newProduct = {
+      id: faker.datatype.uuid(),
+      ...body
+    }
+    this.products.push(newProduct)
+    return newProduct;
+  }
+
+  async find() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.products);
+      }, 5000);
+    });
+  }
+
+  async findOne(id) {
+    const product = this.products.find(item => item.id === id);
+    if(!product){
+      throw boom.notFound('product not found')
+    }
+    if(product.isBlock){
+      throw boom.conflict('product is blocked')
+    }
+    return product;
+  }
+
+  async update(id, changes) {
+    const index = this.products.findIndex(item => item.id === id);
+    if(index === -1){
+      throw boom.notFound('product not found');
+    }else {
+      const product = this.products[index];
+      this.products[index] = {
+        ...product,
+        ...changes
+      };
+      return this.products[index];
+    }
+  }
+
+  async delete(id) {
+    const index = this.products.findIndex(item => item.id === id);
+    if(index === -1){
+      throw boom.notFound('product not found');
+    }else {
+      this.products.splice(index, 1);
+      return {id};
+    }
+  }
+}
+
+module.exports = ProductService;
